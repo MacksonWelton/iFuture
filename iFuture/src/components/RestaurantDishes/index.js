d488ff31dea66d8d8ed19077f3b00d3cb006fb4d
MacksonWelton <mackson.welton@gmail.com>
2020-07-14 09:44:50 -0300

    Correção para mostrar titulo acompanhamentos apenas quando existir companhamentos

--- iFuture/src/components/RestaurantDishes/index.js
+++ iFuture/src/components/RestaurantDishes/index.js
@@ -5,24 +5,25 @@ import CardFood from '../CardFood';
 
 function RestaurantDishes(props) {
     const { data } = props
-   
+
     return (
         <div>
             <S.Title> Principais </S.Title>
 
             {data.products
-            .filter(item => item.category !== 'Acompanhamento')
-            .map(item => (
-                <CardFood key={item.id} item={item} restaurantId={data.id} />
-            ))}
+                .filter(item => item.category !== 'Acompanhamento')
+                .map(item => (
+                    <CardFood key={item.id} item={item} restaurantId={data.id} />
+                ))}
 
-            <S.Title> Acompanhamentos </S.Title>
+            {data.products.filter(item => item.category === 'Acompanhamento').length > 0 ?
+                <S.Title> Acompanhamentos </S.Title> : false}
 
             {data.products
-            .filter(item => item.category === 'Acompanhamento')
-            .map(item => (
-                <CardFood key={item.id} item={item} restaurantId={data.id} />
-            ))}
+                .filter(item => item.category === 'Acompanhamento')
+                .map(item => (
+                    <CardFood key={item.id} item={item} restaurantId={data.id} />
+                ))}
         </div>
     )
 }
