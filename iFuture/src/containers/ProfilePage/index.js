0e92e50561a3eb6adb528f2186953aac88a8f10d
MacksonWelton <mackson.welton@gmail.com>
2020-07-14 09:55:13 -0300

    Correção na página de atributo na página de perfil

--- iFuture/src/containers/ProfilePage/index.js
+++ iFuture/src/containers/ProfilePage/index.js
@@ -35,7 +35,7 @@ function ProfilePage(props) {
         <S.IconEdit src={require("../../assets/edit.svg")} alt='Voltar' onClick={goToProfileEdit} />
 
         <S.AdressContainer>
-          <S.TextStyled grey> Endereço cadastrado </S.TextStyled>
+          <S.TextStyled grey="true"> Endereço cadastrado </S.TextStyled>
           <S.TextStyled> {user.address} </S.TextStyled>
           <S.IconEdit src={require("../../assets/edit.svg")} alt='Voltar' onClick={goToAdressEdit} />
         </S.AdressContainer>
