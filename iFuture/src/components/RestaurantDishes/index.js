7d05cacc1a8855356140d80c35d64fe0e8b77741
MacksonWelton <mackson.welton@gmail.com>
2020-07-14 09:30:00 -0300

    Correção de erro de ausência de key

--- iFuture/src/components/RestaurantDishes/index.js
+++ iFuture/src/components/RestaurantDishes/index.js
@@ -13,7 +13,7 @@ function RestaurantDishes(props) {
             {data.products
             .filter(item => item.category !== 'Acompanhamento')
             .map(item => (
-                <CardFood item={item} restaurantId={data.id} />
+                <CardFood key={item.id} item={item} restaurantId={data.id} />
             ))}
 
             <S.Title> Acompanhamentos </S.Title>
@@ -21,7 +21,7 @@ function RestaurantDishes(props) {
             {data.products
             .filter(item => item.category === 'Acompanhamento')
             .map(item => (
-                <CardFood item={item} restaurantId={data.id} />
+                <CardFood key={item.id} item={item} restaurantId={data.id} />
             ))}
         </div>
     )
